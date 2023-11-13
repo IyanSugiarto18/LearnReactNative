a2eb799b41954e8db56e650db85af7e5ac7956fa
IyanSugiarto <IyanSugiarto18@gmail.com>
2023-11-13 08:41:33 +0700

    props in ComponentText

--- src/Components/Text/CompponentText.js
+++ src/Components/Text/CompponentText.js
@@ -2,11 +2,20 @@ import React from "react";
 import {Text, View} from "react-native";
 import style from "../../assets/style";
 
+const Nama = props => {
+    return (
+        <View>
+            <Text style={style.text}>Hello Wolrd! {props.name}</Text>
+        </View>
+    );
+};
 
 const ComponentText = () => {
     return (
         <View style={style.container}>
-            <Text style={style.text}>Hello Wolrd! anjing banget ya kamu</Text>
+            <Nama name="Iyan Sugiarto"></Nama>
+            <Nama name="Asyifa Nur Alif"></Nama>
+
         </View>
     );
 };
